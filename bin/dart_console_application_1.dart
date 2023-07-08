24ca816a6857cecd5a96fb5838e1817ca3b0c737
ghajavarestani <ermiailia@gmail>
2023-07-08 18:57:11 +0330

    create file 8th

---
+++ bin/dart_console_application_1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_console_application_1/dart_console_application_1.dart' as dart_console_application_1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_console_application_1.calculate()}!');
+}
