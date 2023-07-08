ff497a96643d01b4598c04d5743d696644afa7cf
ghajavarestani <ermiailia@gmail>
2023-07-08 19:05:41 +0330

    add count variable with type int

--- bin/dart_console_application_1.dart
+++ bin/dart_console_application_1.dart
@@ -1,5 +1,10 @@
-import 'package:dart_console_application_1/dart_console_application_1.dart' as dart_console_application_1;
+import 'package:dart_console_application_1/dart_console_application_1.dart'
+    as dart_console_application_1;
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_console_application_1.calculate()}!');
+  int count;
+  count = 7;
+  print('count = $count');
+  
 }
