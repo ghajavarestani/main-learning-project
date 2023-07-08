4b75a50b90619a4ff88470c9f20d8b2ba358cfdd
ghajavarestani <ermiailia@gmail>
2023-07-08 19:42:01 +0330

    add print ====***===

--- bin/dart_console_application_1.dart
+++ bin/dart_console_application_1.dart
@@ -3,16 +3,33 @@ import 'package:dart_console_application_1/dart_console_application_1.dart'
 
 void main(List<String> arguments) {
   print('Hello world: ${dart_console_application_1.calculate()}!');
+  print('==========*****==========');
+
   int count;
   count = 7;
   print('count = $count');
+  print('==========*****==========');
+
   int myAge;
   int age2;
   //age2 = 3.14;
   double pi = 3.14;
   print('pi = $pi');
+  print('==========*****==========');
+
   int a = 9;
   int A = 9;
+  print('==========*****==========');
+
   String name = 'Flutter';
   print('name = $name');
+  print('==========*****==========');
+
+  bool isOk = false;
+  print('isOk = $isOk');
+  isOk = true;
+  print('isOk = $isOk');
+  print('==========*****==========');
+
+  
 }
