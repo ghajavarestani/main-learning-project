24f0ffe9abd9039004e68bd84afb8c03446d9628
ghajavarestani <ermiailia@gmail>
2023-07-10 20:53:16 +0330

    shoroe fim 19th functions in dar saat ketabkhane tamoum shod

--- bin/my_functionsFilm19thFunctions.dart
+++ bin/my_functionsFilm19thFunctions.dart
@@ -1,7 +1,28 @@
 void main(List<String> args) {
   //film 19th functions in dart
-    
-    print('z is equals $z');
-    print('z equals to $z');
+  hello();
+  hello1();
+  createNumber();
   print('==========#####==========');
+  print('createNumber is equals $createNumber');
+  print('createNumber equals to $createNumber');
+  print('hello1 is equals $hello1');
+  print('hello1 equals to $hello1');
+  print('hello is equals $hello');
+  print('hello equals to $hello');
+  print('==========#####==========');
+}
+
+createNumber() {
+  print('new number is 100');
+}
+
+hello() {
+  //yek function neveshtim ke na voroudi dare na khorouji
+}
+hello1() {
+  print('hello world');
+  createNumber();
+  print('==========#####==========');
+  //yek function neveshtim ke voroudi nadare vali khorouji dare
 }
